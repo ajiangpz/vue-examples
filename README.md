fd2e6d7e22b31a397137f8d9cd8898905ab5e0a8
jiangpengzhen <jiangpengzhen@tenda.cn>
2023-12-29 17:44:58 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-examples
-vue-examples
---
+++ vue-project/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-config-provider>
+    <router-view></router-view>
+  </el-config-provider>
+</template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<style lang="scss">
+</style>
